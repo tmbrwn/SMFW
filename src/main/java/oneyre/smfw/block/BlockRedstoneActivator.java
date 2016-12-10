8502dbe46bac750872e31e2fc00da42f014427cd
Tim Brown <tmbrwn11@gmail.com>
2016-12-10 14:04:54 -0500

    fixed redstone activator rendering

--- src/main/java/oneyre/smfw/block/BlockRedstoneActivator.java
+++ src/main/java/oneyre/smfw/block/BlockRedstoneActivator.java
@@ -18,100 +18,110 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+/**
+ * An invisible block which provides power to blocks below it. Takes 20 ticks to
+ * decay if the IS_PLAYER_IN property is not set externally.
+ * 
+ * @author Oneyre
+ *
+ */
 public class BlockRedstoneActivator extends Block {
-	
+
 	public static final IProperty<Boolean> IS_PLAYER_IN = PropertyBool.create("is_player_in");
 	public static final int TICKS_TO_UPDATE = 5;
-	
+	public static final int POWER_LEVEL = 4;
+
 	public BlockRedstoneActivator() {
 		super(new MaterialTransparent(MapColor.AIR));
 		IBlockState defaultBlockState = this.blockState.getBaseState().withProperty(IS_PLAYER_IN, true);
 		setDefaultState(defaultBlockState);
 	}
-	
+
+	/**
+	 * Reminder: Instantiates IBlockState objects for all combinations of values for
+	 * associated IProperties. Must override this to use IProperties
+	 */
 	@Override
 	public BlockStateContainer createBlockState() {
 		return new BlockStateContainer(this, IS_PLAYER_IN);
 	}
-	
+
 	@Override
 	public int getMetaFromState(IBlockState state) {
 		return state.getValue(IS_PLAYER_IN) ? 1 : 0;
 	}
-	
-	@Override 
+
+	@Override
 	public IBlockState getStateFromMeta(int meta) {
-		if(meta == 0)
+		if (meta == 0)
 			return this.getDefaultState().withProperty(IS_PLAYER_IN, false);
 		else
 			return this.getDefaultState();
 	}
 
 	@Override
-	public boolean canProvidePower(IBlockState state)
-    {
-        return true;
-    }
+	public boolean canProvidePower(IBlockState state) {
+		return true;
+	}
 
 	@Override
 	public int getWeakPower(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
-		if(side == EnumFacing.UP)
-			return 15;
+		if (side == EnumFacing.UP)
+			return POWER_LEVEL;
 		return 0;
 	}
-	
-	/*@Override
-	public int getStrongPower(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
-		return 15;
-	}*/
-	
+
+	/*
+	 * @Override public int getStrongPower(IBlockState blockState, IBlockAccess
+	 * blockAccess, BlockPos pos, EnumFacing side) { return 15; }
+	 */
+
 	@Override
 	public EnumBlockRenderType getRenderType(IBlockState iBlockState) {
-	    return EnumBlockRenderType.INVISIBLE;
+		return EnumBlockRenderType.INVISIBLE;
 	}
-	
+
 	@Override
 	@Nullable
-    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, World worldIn, BlockPos pos)
-    {
-        return NULL_AABB;
-    }
-	
+	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, World worldIn, BlockPos pos) {
+		return NULL_AABB;
+	}
+
 	@Override
-	public boolean canCollideCheck(IBlockState state, boolean hitIfLiquid)
-    {
-        return false;
-    }
-	
+	public boolean canCollideCheck(IBlockState state, boolean hitIfLiquid) {
+		return false;
+	}
+
 	@Override
-	public boolean isReplaceable(IBlockAccess worldIn, BlockPos pos)
-    {
-        return true;
-    }
-	
+	public boolean isReplaceable(IBlockAccess worldIn, BlockPos pos) {
+		return true;
+	}
+
 	@Override
-	public boolean isFullCube(IBlockState state)
-    {
-        return false;
-    }
-	
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
 	@Override
 	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
-		if(worldIn.isRemote)
+		if (worldIn.isRemote)
 			return;
-		if(state.getValue(IS_PLAYER_IN)) {
+		if (state.getValue(IS_PLAYER_IN)) {
 			worldIn.setBlockState(pos, state.withProperty(IS_PLAYER_IN, false));
 			worldIn.scheduleUpdate(pos, state.getBlock(), TICKS_TO_UPDATE);
-		}
-		else
+		} else
 			worldIn.setBlockToAir(pos);
-			
+
 	}
-	
+
 	@Override
-	public boolean canConnectRedstone(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side)
-    {
-        return false;
-    }
+	public boolean canConnectRedstone(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side) {
+		return false;
+	}
 
 }
--- src/main/java/oneyre/smfw/item/ItemRedstoneArmor.java
+++ src/main/java/oneyre/smfw/item/ItemRedstoneArmor.java
@@ -15,41 +15,38 @@ import oneyre.smfw.block.BlockRedstoneActivator;
 import oneyre.smfw.init.SmfwBlocks;
 import oneyre.smfw.init.SmfwItems;
 
+/**
+ * Armor made of redstone which, when worn on the feet, activates redstone
+ * devices you walk on.
+ * 
+ * @author Oneyre
+ *
+ */
 public final class ItemRedstoneArmor extends ItemArmor {
-	
-	private static ArmorMaterial redstoneMaterial = EnumHelper.addArmorMaterial(
-			"Redstone",
-			Smfw.MODID + ":redstone",
-			10,
-			new int[]{1,3,4,1},
-			15,
-			SoundEvents.ITEM_ARMOR_EQUIP_GOLD,
-			0.0F
-		);
-	
+
+	private static ArmorMaterial redstoneMaterial = EnumHelper.addArmorMaterial("Redstone", Smfw.MODID + ":redstone",
+			10, new int[] { 1, 3, 4, 1 }, 15, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0.0F);
+
 	public ItemRedstoneArmor(EntityEquipmentSlot slot) {
 		super(redstoneMaterial, -1, slot);
 	}
-	
+
 	@Override
 	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack) {
-		if(world.isRemote || !itemStack.getItem().equals(SmfwItems.redstoneArmorFeet))
+		if (world.isRemote || !itemStack.getItem().equals(SmfwItems.redstoneArmorFeet))
 			return;
 		BlockPos feetPos = player.getPosition();
 		IBlockState blockInFoot = world.getBlockState(feetPos);
 		IBlockState blockInHead = world.getBlockState(feetPos.up());
-		if(blockInFoot.getBlock() == Blocks.AIR) {
+		if (blockInFoot.getBlock() == Blocks.AIR) {
 			world.setBlockState(feetPos, SmfwBlocks.redstoneActivator.getDefaultState());
 			world.scheduleUpdate(feetPos, SmfwBlocks.redstoneActivator, BlockRedstoneActivator.TICKS_TO_UPDATE);
-		}
-		else if(blockInFoot.getBlock() == SmfwBlocks.redstoneActivator) {
+		} else if (blockInFoot.getBlock() == SmfwBlocks.redstoneActivator) {
 			world.setBlockState(feetPos, blockInFoot.withProperty(BlockRedstoneActivator.IS_PLAYER_IN, true));
-		}
-		else if(blockInHead.getBlock() == Blocks.AIR) {
+		} else if (blockInHead.getBlock() == Blocks.AIR) {
 			world.setBlockState(feetPos.up(), SmfwBlocks.redstoneActivator.getDefaultState());
 			world.scheduleUpdate(feetPos.up(), SmfwBlocks.redstoneActivator, BlockRedstoneActivator.TICKS_TO_UPDATE);
-		}
-		else if(blockInHead.getBlock() == SmfwBlocks.redstoneActivator) {
+		} else if (blockInHead.getBlock() == SmfwBlocks.redstoneActivator) {
 			world.setBlockState(feetPos.up(), blockInHead.withProperty(BlockRedstoneActivator.IS_PLAYER_IN, true));
 		}
 	}
